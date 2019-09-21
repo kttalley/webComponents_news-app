60b42991cab5faca93573c424f2ba373715c87d2
kttalley <talleykristian@gmail.com>
2019-09-21 12:40:38 -0700

    seperated out attempt successful

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '3f91d51a212b4a0aa198da194e97655b';
+const topHeadlinesUrl =
+    'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };

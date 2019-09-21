1acc6cb1764be8f3461ccff16102b228f2c15002
kttalley <talleykristian@gmail.com>
2019-09-21 12:45:59 -0700

    removing export/import statements working as intended

--- newsApi.js
+++
@@ -1,5 +0,0 @@
-const apiKey = '3f91d51a212b4a0aa198da194e97655b';
-const topHeadlinesUrl =
-    'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
-
-export { apiKey, topHeadlinesUrl };
